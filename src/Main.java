9f9bebf5e38b196dfbb0df536b58aae4afaa5c6b
Karol Jaramillo <karoljaramillo195@gmail.com>
2025-04-25 11:03:42 -0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("¡Hola desde IntelliJ y GitHub!");
-        #ytdtfgfhyg
-}
+     public static void main(String[] args) {
+        byte edad = 20;
+        short numeroDepartamento = 204;
+        int numeroTorre = 1000;
+        long numeroCelular = 61857443;
+        float altemperatura = 36.5f;
+        double altura = 1.80;
+        boolean esVisitante = true;
+        char inicialNombre = 'K';
+        String nombre = "Karol";
+        String apellido = "Jaramillo";
+        String nombreCompleto = nombre + " " + apellido;
+
+
+        System.out.println("Inicial del nombre: " + inicialNombre);
+        System.out.println("Nombre: " + nombreCompleto);
+        System.out.println("Edad: " + edad);
+        System.out.println("Número de departamento: " + numeroDepartamento);
+        System.out.println("Número de torre: " + numeroTorre);
+        System.out.println("Número de celular: " + numeroCelular);
+        System.out.println("Altura: " + altura);
+        System.out.println("Temperatura: " + altemperatura);
+        System.out.println("Es visitante: " + esVisitante);
+    }
 }
